4b52ead3156fd4f09c348c8c68c9fedb6c336266
ashleyyy <ashleyfisher@gmail.com>
2018-04-17 11:24:26 -0700

    test js

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
   $('#signout').on("click", function () {
     console.log('in signout onclick')
-    // $.get( "https://yoursite.thinkific.com/users/sign_out" )
+    $.get( "https://yoursite.thinkific.com/users/sign_out" )
   })
 
 
